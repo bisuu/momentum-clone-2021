68b178b8226cc58023fc47ae308fdf10e380ff55
bisuu <limbo.code.97@gmail.com>
2023-01-15 14:43:36 +0000

    Loading Username

--- app.js
+++ app.js
@@ -5,15 +5,30 @@ const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value; 
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     console.log(username);
-    greeting.innerText= `Hello ${username}`; 
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`; 
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUsername);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
