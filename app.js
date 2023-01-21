60fad5f182aab6f5f52b08c8270c5d1fe9325ba3
bisuu <limbo.code.97@gmail.com>
2023-01-21 06:07:32 +0000

    Super Recap

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const username = loginInput.value; 
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value; 
     localStorage.setItem(USERNAME_KEY, username);
-    console.log(username);
     paintGreetings(username);
 }
-
 
 function paintGreetings(username) {
     greeting.classList.remove(HIDDEN_CLASSNAME);
@@ -23,8 +21,6 @@ function paintGreetings(username) {
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-console.log(savedUsername);
 
 if(savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
