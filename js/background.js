f974b2ee7cd31a947e5f8f1e17dca546f54d7ebd
bisuu <limbo.code.97@gmail.com>
2023-01-28 09:46:11 +0000

    Center

--- js/background.js
+++ js/background.js
@@ -3,6 +3,6 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
 const body = document.body; 
-const bgImage =  `url(img/${chosenImage})`;
+const bgImage =  `url('img/${chosenImage}')`;
 
 body.style.backgroundImage = bgImage;
